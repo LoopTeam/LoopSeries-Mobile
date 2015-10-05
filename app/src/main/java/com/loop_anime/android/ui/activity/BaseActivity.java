a65a532ddca7dcffca83064925525dee1aac617a
yilun <allan1st@gmail.com>
2015-10-05 01:06:47 +0100

    Update Theme

--- app/src/main/java/com/loop_anime/android/ui/activity/BaseActivity.java
+++ app/src/main/java/com/loop_anime/android/ui/activity/BaseActivity.java
@@ -3,9 +3,6 @@ package com.loop_anime.android.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.loop_anime.android.R;
-import com.loop_anime.android.utils.ColorUtils;
-
 import rx.subscriptions.CompositeSubscription;
 
 /**
@@ -20,7 +17,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setNavigationBarColor(ColorUtils.getColor(this, R.color.colorPrimaryDark));
         mCompositeSubscription = new CompositeSubscription();
     }
 
