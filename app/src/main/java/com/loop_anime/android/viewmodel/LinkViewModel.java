9ec2da1d4e358f943a5e445bc4ad221ff6836a25
yilun <allan1st@gmail.com>
2015-11-04 16:59:16 +0000

    Resolve null pointer exception

--- app/src/main/java/com/loop_anime/android/viewmodel/LinkViewModel.java
+++ app/src/main/java/com/loop_anime/android/viewmodel/LinkViewModel.java
@@ -1,17 +1,18 @@
 package com.loop_anime.android.viewmodel;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.loop_anime.android.R;
 import com.loop_anime.android.model.api.API;
+import com.loop_anime.android.model.dao.DirectLink;
 import com.loop_anime.android.model.dao.Link;
 import com.loop_anime.android.ui.activity.BaseActivity;
 import com.loop_anime.android.utils.VideoUtils;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Subscription;
@@ -51,11 +52,11 @@ public class LinkViewModel {
                     .timeout(5, TimeUnit.SECONDS)
                     .subscribe(directLink -> {
                         //launch with external player
-                        List<String> dq = directLink.getDirectLink().getDq();
-                        if (dq != null && dq.size() > 0) {
-                            VideoUtils.launchVideoURLWithExternalPlayer(view.getContext(), dq.get(0));
+                        DirectLink.DirectLinkEntity dl = directLink.getDirectLink();
+                        if (dl != null && dl.getDq() != null && dl.getDq().size() > 0) {
+                            VideoUtils.launchVideoURLWithExternalPlayer(view.getContext(), dl.getDq().get(0));
                         } else {
-                            Toast.makeText(view.getContext(), R.string.error_no_link, Toast.LENGTH_SHORT).show();
+                            Snackbar.make(view, R.string.error_no_link, Snackbar.LENGTH_SHORT).show();
                         }
                     }, throwable -> {
                         Log.e(LOG_TAG, throwable.getMessage());
